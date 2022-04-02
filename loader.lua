9518a1a51f43f2bfb29bcaa3d5ca8023c8073daf
synolope <98623199+synolope@users.noreply.github.com>
2022-04-01 21:03:34 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,8 @@
-local loaderurl = ""
+local loaderurl = "https://raw.githubusercontent.com/synolope/mpcity/main/loader.lua?token=GHSAT0AAAAAABTEMCA5SW6ONH2QFWSEKJXMYSHUCQQ"
 local scripturl = "https://raw.githubusercontent.com/synolope/mpcity/main/script.lua?token=GHSAT0AAAAAABTEMCA54XOVJHPRZVQ2YEKSYSHUAQA"
+loadstring(game:HttpGet(scripturl,true))()
+
+syn.queue_on_teleport([[
+    wait(3)
+    loadstring(game:HttpGet("]] .. loaderurl .. [[",true))()
+]])
