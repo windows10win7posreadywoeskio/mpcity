8d87b5b51287a68dcd2c538df7d6852d47da56c1
synolope <98623199+synolope@users.noreply.github.com>
2022-04-10 21:27:18 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -246,6 +246,12 @@ Servers:AddButton("Join Most Populated Server",function()
 	joinserver(server.InstanceId)
 end)
 
+Servers:AddButton("Join Random Server",function()
+	local servers = getservers()
+	local server = servers[math.random(1, #servers)]
+	joinserver(server.InstanceId)
+end)
+
 local function rnd()
 	return math.random(-25,25)
 end
