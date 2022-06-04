aaf33f33128fdc2db469227e3b8e9da8e6a363e8
ha beep <56192384+windows10win7posreadywoeskio@users.noreply.github.com>
2022-06-04 11:35:57 +0700

    BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP

--- loader.lua
+++ loader.lua
@@ -29,8 +29,11 @@ if game.CreatorId == 123247 or game.PlaceId == 370731277 and continuee == true t
 	if identifyexecutor() == "Synapse X" then
 		scripturl = githublink .. "synapse.lua"
 	end
-	if identifyexecutor() == "Krnl" or queue_on_teleport or "Fluxus" then
+	if identifyexecutor() == "Krnl" or queue_on_teleport then
 		scripturl = githublink .. "krnl.lua"
+	end
+	if identifyexecutor() == "Fluxus" then
+		scripturl = githublink .. "mainscript.lua"
 	end
 	queueOnTeleport([[
     		wait(3)
